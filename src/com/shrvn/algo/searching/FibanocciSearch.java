26707ed7669ff5386e8181ef5106344929db0a37
Shra012 <Shravan_fisher@yahoo.ca>
2019-05-14 03:32:46 +0530

    completed the implementation of Fibanocci search.

--- src/com/shrvn/algo/searching/FibanocciSearch.java
+++ src/com/shrvn/algo/searching/FibanocciSearch.java
@@ -1,6 +1,8 @@
 package com.shrvn.algo.searching;
 
 
+import java.util.Arrays;
+
 /**
  *  Input:  arr[] = {2, 3, 4, 10, 40}, x = 10
 	Output:  3
@@ -13,18 +15,58 @@ package com.shrvn.algo.searching;
  *
  */
 public class FibanocciSearch {
-
+    int [] fibanocci;
 	public static void main(String[] args) {
 		int arr[] = {2, 3, 4, 10, 40};
 		FibanocciSearch search = new FibanocciSearch();
-		search.doSearch(arr);
+        System.out.println(search.doSearch(arr,4));
 	}
 
-	public int doSearch(int[] arr){
+	public int doSearch(int[] arr,int x){
 		int length = arr.length;
+		getNearestFibannoci(length);
+        int fibMMm2 = fibanocci[fibanocci.length-3]; // (m-2)'th Fibonacci No.
+        int fibMMm1 = fibanocci[fibanocci.length-2]; // (m-1)'th Fibonacci No.
+        int fibM = fibanocci[fibanocci.length-1];
+        int offset = -1;
+        while (fibM > 1)
+        {
+            // Check if fibMm2 is a valid location
+            int i = min(offset+fibMMm2, length-1);
 
-		return 0;
+            /* If x is greater than the value at
+            index fibMm2, cut the subarray array
+            from offset to i */
+            if (arr[i] < x)
+            {
+                fibM = fibMMm1;
+                fibMMm1 = fibMMm2;
+                fibMMm2 = fibM - fibMMm1;
+                offset = i;
+            }
+
+            /* If x is greater than the value at index
+            fibMm2, cut the subarray after i+1 */
+            else if (arr[i] > x)
+            {
+                fibM = fibMMm2;
+                fibMMm1 = fibMMm1 - fibMMm2;
+                fibMMm2 = fibM - fibMMm1;
+            }
+
+            /* element found. return index */
+            else return i;
+        }
+
+        /* comparing the last element with x */
+        if(fibMMm1 == 1 && arr[offset+1] == x)
+            return offset+1;
+
+        return -1;
 	}
+
+    public static int min(int x, int y)
+    { return (x <= y)? x : y; }
 
     /**
      * Returns the nearest fibanocci number
@@ -43,11 +85,17 @@ public class FibanocciSearch {
      * @return
      */
     public int getNearestFibannoci(int number,int previous,int current){
+        int i=1;
+        fibanocci = new int[i];
         while(number>=current){
             int newCurrent = getNextFibannoci(previous,current);
             previous=current;
             current=newCurrent;
+            fibanocci[i-1]=previous;
+            i++;
+            fibanocci = Arrays.copyOf(fibanocci,i);
         }
+        fibanocci[i-1] = current;
         return current;
     }
 
